22b99253b691aa78613ef99cc453071b542bf4cf
pedrocasi <pedrocs@exo.pt>
2023-01-13 14:58:37 +0000

    ptbot

---
+++ payment_portugal/__manifest__.py
@@ -0,0 +1,33 @@
+##############################################################################
+#
+#    Copyright (C) 2016 Exo Software, Lda. (<https://exosoftware.pt>)
+#
+##############################################################################
+# pylint: disable=license-allowed, manifest-required-author
+{
+    "name": "Payment Acquirer Portugal",
+    "category": "Accounting/Payment Acquirers",
+    "summary": "A base module for portuguese payment methods",
+    "version": "14.0.2.1.0",
+    "author": "Exo Software",
+    "website": "https://exosoftware.pt",
+    "depends": ["payment", "l10n_pt"],
+    "data": [
+        "security/ir.model.access.csv",
+        "data/payment_icon_data.xml",
+        "data/ir_cron.xml",
+        "views/assets.xml",
+        "views/report_templates.xml",
+        "views/report_invoice.xml",
+        "views/report_sale_order.xml",
+        "views/payment_views.xml",
+        "views/account_move_views.xml",
+        "views/res_partner_views.xml",
+        "views/payment_templates.xml",
+        "views/payment_mbway_templates.xml",
+        "wizard/account_generate_multibanco_views.xml",
+    ],
+    "installable": True,
+    "application": False,
+    "license": "OPL-1",
+}
