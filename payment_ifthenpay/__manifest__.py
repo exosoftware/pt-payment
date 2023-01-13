a6728ebb0b187a24f42f3119217f0a52d1cb513d
pedrocasi <pedrocs@exo.pt>
2023-01-13 16:31:13 +0000

    ptplus bot maker

---
+++ payment_ifthenpay/__manifest__.py
@@ -0,0 +1,26 @@
+##############################################################################
+#
+#    Copyright (C) 2016 Exo Software, Lda. (<https://exosoftware.pt>)
+#
+##############################################################################
+# pylint: disable=license-allowed, manifest-required-author
+{
+    "name": "Ifthenpay Payment Acquirer",
+    "category": "Accounting/Payment Acquirers",
+    "sequence": 390,
+    "summary": "Payment Acquirer: Ifthenpay Implementation",
+    "version": "14.0.2.1.0",
+    "author": "Exo Software",
+    "website": "https://exosoftware.pt",
+    "depends": ["payment_portugal"],
+    "data": [
+        "views/payment_views.xml",
+        "views/payment_ifthenpay_templates.xml",
+        "data/payment_acquirer_data.xml",
+    ],
+    "installable": True,
+    "application": True,
+    "post_init_hook": "create_missing_journal_for_acquirers",
+    "uninstall_hook": "uninstall_hook",
+    "license": "OPL-1",
+}
